41200cf6195442d928995e5bdd03491b27b397c5
Nico Carlier <carlier.nicholas@gmail.com>
2025-06-07 18:52:56 -0700

    fix: only drop DB in development environment

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.routes import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
 from app.db import init_db, reset_db
+import os
 
 app = FastAPI()
 
@@ -20,8 +21,9 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def on_startup():
-    # Use reset_db() to recreate tables with new UUID schema
-    await reset_db()
-    # After first run, you can change this back to: await init_db()
+    if os.getenv("ENV") == "dev":
+        await reset_db()
+    else:
+        await init_db()
 
 app.include_router(api_router, prefix="/api")
